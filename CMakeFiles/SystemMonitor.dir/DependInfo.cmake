d3e3c4c0f667bedc48c851e4e9388be7e6dcd025
ttsudarshan <sudarshantiwary09@gmail.com>
2026-03-11 00:20:55 -0500

    week 2

---
+++ CMakeFiles/SystemMonitor.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "SystemMonitor_autogen/timestamp" "custom" "SystemMonitor_autogen/deps"
+  "/home/sudarshan/system-monitor/build/SystemMonitor_autogen/mocs_compilation.cpp" "CMakeFiles/SystemMonitor.dir/SystemMonitor_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/SystemMonitor_autogen/mocs_compilation.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/MainWindow.cpp" "CMakeFiles/SystemMonitor.dir/src/MainWindow.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/MainWindow.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/main.cpp" "CMakeFiles/SystemMonitor.dir/src/main.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/main.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/monitors/CpuMonitor.cpp" "CMakeFiles/SystemMonitor.dir/src/monitors/CpuMonitor.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/monitors/CpuMonitor.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/monitors/MemoryMonitor.cpp" "CMakeFiles/SystemMonitor.dir/src/monitors/MemoryMonitor.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/monitors/MemoryMonitor.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/widgets/LogsTab.cpp" "CMakeFiles/SystemMonitor.dir/src/widgets/LogsTab.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/widgets/LogsTab.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/widgets/MetricsTab.cpp" "CMakeFiles/SystemMonitor.dir/src/widgets/MetricsTab.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/widgets/MetricsTab.cpp.o.d"
+  "/home/sudarshan/system-monitor/src/widgets/ProcessesTab.cpp" "CMakeFiles/SystemMonitor.dir/src/widgets/ProcessesTab.cpp.o" "gcc" "CMakeFiles/SystemMonitor.dir/src/widgets/ProcessesTab.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/SystemMonitor.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SystemMonitor_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/SystemMonitor_autogen.dir/ParseCache.txt"
+  "SystemMonitor_autogen"
+  "CMakeFiles/SystemMonitor.dir/SystemMonitor_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/SystemMonitor_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/MainWindow.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/MainWindow.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/main.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/main.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/monitors/CpuMonitor.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/monitors/CpuMonitor.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/monitors/MemoryMonitor.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/monitors/MemoryMonitor.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/widgets/LogsTab.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/widgets/LogsTab.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/widgets/MetricsTab.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/widgets/MetricsTab.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/widgets/ProcessesTab.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/widgets/ProcessesTab.cpp.o.d"
+  "SystemMonitor"
+  "SystemMonitor.pdb"
+  "SystemMonitor_autogen/mocs_compilation.cpp"
+  "SystemMonitor_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SystemMonitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
